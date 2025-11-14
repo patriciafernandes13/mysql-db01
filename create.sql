53b5cb9c5456d17bd8b7fd2f3b346088ab40bf90
Patrícia Fernandes <tita1099@gmail.com>
2025-11-14 10:46:31 +0000

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,1 +1,7 @@
-create table hello(int id, char(50) nome)
+CREATE TABLE `livros` (
+  `livro_id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `codigo` varchar(45) NOT NULL,
+  PRIMARY KEY (`livro_id`),
+  UNIQUE KEY `codigo_UNIQUE` (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
